aac8b2f8fe1110ba021abfa145e01dd15f962804
진섭 <hwangjs0823@naver.com>
2025-12-10 19:08:27 +0900

    돼라

--- app/cat/page.tsx
+++ app/cat/page.tsx
@@ -1,6 +1,6 @@
 // app/cat/page.tsx
 import { loadAllCats } from "@/lib/catsLoader";
-import CatPage from "@/components/cat/CatPage";
+import CatPage from "@/app/cat/CatPage";
 
 export default async function Page() {
   const cats = await loadAllCats();      // SSR + 파일 읽기
