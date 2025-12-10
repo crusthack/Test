1988c485c0200eaafbc6e7d0ac9ff1e66e45773f
진섭 <hwangjs0823@naver.com>
2025-12-10 18:52:18 +0900

    dd

--- app/cat/[id]/page.tsx
+++ app/cat/[id]/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-dynamic";
-
 import { notFound } from "next/navigation";
 import { loadCatsById, loadAllCats } from "@/lib/catsLoader";
 import Image from "next/image";
